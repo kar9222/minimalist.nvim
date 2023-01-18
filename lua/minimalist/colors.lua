07afc3dbe4bce6c16cd42bce0b25a2c81ee853cf
kar9222 <karchua922@gmail.com>
2023-01-18 21:12:31 +1100

    Change colors for search to make it brighter

--- lua/minimalist/colors.lua
+++ lua/minimalist/colors.lua
@@ -25,8 +25,8 @@ local minimalist = {
   standout_special_0 = "#dad49c",   -- Markdown title, section title, etc
   standout_special_1 = "#C7C7BE",   -- juliaKeyword, etc
   standout_special_1_2 = "#dadad4",   -- juliaMacro
-  standout_special_2 = "#1f454f",   -- Find match bg, etc
-  standout_special_2_2 = "#2e6676", -- Find match IncSearch, etc
+  standout_special_2 = "#2e6676",   -- Find match bg, etc (prev color #1f454f)
+  standout_special_2_2 = "#A56969", -- Find match IncSearch, etc (prev color #2e6676)
   standout_special_3 = "#C7C7BF",   -- Keyword, etc
 
   fold_bg = '#31343C',
